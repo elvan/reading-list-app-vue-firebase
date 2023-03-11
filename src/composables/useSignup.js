f289710cf63651a504b94f01e214c1c4c28c092f
Elvan Hidayat <elvanhd@gmail.com>
2023-03-11 14:10:06 +0700

    signing up new users

---
+++ src/composables/useSignup.js
@@ -0,0 +1,33 @@
+import { ref } from 'vue';
+
+// firebase imports
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../firebase/config';
+
+const error = ref(null);
+const isPending = ref(false);
+
+const signup = async (email, password) => {
+  error.value = null;
+  isPending.value = true;
+
+  try {
+    const res = await createUserWithEmailAndPassword(auth, email, password);
+    if (!res) {
+      throw new Error('Could not complete signup');
+    }
+
+    error.value = null;
+    isPending.value = false;
+  } catch (err) {
+    console.log(err.message);
+    error.value = err.message;
+    isPending.value = false;
+  }
+};
+
+const useSignup = () => {
+  return { error, isPending, signup };
+};
+
+export default useSignup;
