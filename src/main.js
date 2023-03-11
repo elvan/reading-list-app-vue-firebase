ab455f820fec38f2cc94936f028c8a9a48210297
Elvan Hidayat <elvanhd@gmail.com>
2023-03-11 14:39:38 +0700

    waiting for auth to be ready

--- src/main.js
+++ src/main.js
@@ -2,4 +2,14 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 
-createApp(App).use(router).mount('#app');
+// firebase imports
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from './firebase/config';
+
+let app;
+
+onAuthStateChanged(auth, () => {
+  if (!app) {
+    app = createApp(App).use(router).mount('#app');
+  }
+});
